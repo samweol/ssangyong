0f3fcdf2b83381fd63d2b297b88405abedf415a3
samweol <sejuyes@gmail.com>
2022-07-29 09:48:53 +0900

    Spring Non Tiles

--- SpringFile/localhost.hr1.sql
+++ SpringFile/localhost.hr1.sql
@@ -7,9 +7,21 @@ create table tblFileBoard (
 );
 
 -- 첨부 파일
+drop table tblFile;
+
 create table tblFile (
     seq number primary key,
     filename varchar2(100) not null,
     filesize varchar2(30) not null,
-    mimetype varchar2(50) not null
+    mimetype varchar2(50) not null,
+    bseq number not null references tblFileBoard(seq)
 );
+
+create sequence seqFileBoard;
+create sequence seqFile;
+
+commit;
+
+select * from tblFileBoard;
+
+select * from tblFile;
---
+++ SpringTiles/src/main/webapp/WEB-INF/views/inc/mainmenu.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+
+<!-- mainmenu.jsp -->
+<div id="mainmenu">
+	<a href="/spring/member/info">회원</a>
+	<a href="/spring/admin/chart">관리자</a>
+</div>
