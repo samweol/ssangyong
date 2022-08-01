94cf8f83de2d9e7cecd60971228d73fcc2c2408c
samweol <sejuyes@gmail.com>
2022-08-01 09:46:41 +0900

    aop

---
+++ SpringAOP/src/main/java/com/test/spring/AOPController.java
@@ -0,0 +1,16 @@
+package com.test.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AOPController {
+
+	@GetMapping(value="/test")
+	private String test() {
+		
+		System.out.println("주업무 : test ");
+		
+		return "test";
+	}
+}
